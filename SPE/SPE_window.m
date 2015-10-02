894e4089ce4a1ea1b3db28362b1a99025037f7b5
mihailovms <mihailovms@mail.ru>
2015-10-02 20:08:14 +0300

    Доделал кнопки на боковой панели + callacks
    Доделал кнопки на боковой панели + callacks

--- SPE/SPE_window.m
+++ SPE/SPE_window.m
@@ -13,8 +13,16 @@ MW.handle = figure(...
 MW = SizeMW(MW);
 MW = PanelPB(MW);
 MW.CounterPB = 0;
+%% <--- кнопки боковой панели
 namePB = 'new';
-MW = NewPB(MW,namePB);
+    MW = NewPB(MW,namePB);
+namePB = 'new2';
+    MW = NewPB(MW,namePB);
+namePB = 'new3';
+    MW = NewPB(MW,namePB);
+%%   
+SetActivePG(0,0,MW, 1)
+ButtonDownCallback(MW);    
 
 function MW = SizeMW(MW)
 scrSize = get(0,'ScreenSize');
@@ -35,41 +43,53 @@ MW.panelPosition = ...
     [MW.panelPB.width+2*MW.panelPB.d, MW.panelPB.d, ...
     MW.win.width-MW.panelPB.width-3*MW.panelPB.d,...
     MW.panelPB.height];
+MW.sizePB.height = 20;
+MW.sizePB.width = 150;
+MW.panelPB.backgroundcolor = [4,5,6]/10;
+MW.panel.backgroundcolor    = [8,7.5,7]/10;
 
 function MW = PanelPB(MW)
 MW.panelPB.position = [MW.panelPB.d, MW.panelPB.d, ...
     MW.panelPB.width, MW.win.height-2*MW.panelPB.d];
-MW.panelPB.backgroundcolor = [4,5,6]/10;
 MW.panelPB.handle = uipanel(MW.handle, 'Units', 'pixels', ...
     'Position',  MW.panelPB.position, 'BorderType', 'none', ...
 	'BackgroundColor', MW.panelPB.backgroundcolor);
 
 function MW = NewPB(MW, name) 
 MW.CounterPB = MW.CounterPB + 1;
-MW.panel.backgroundcolor = [8,7.5,7]/10;
 MW.PG{MW.CounterPB}.handle = uipanel(MW.handle, 'Title', name, ... 
                 'BackgroundColor', MW.panel.backgroundcolor );
 set(MW.PG{MW.CounterPB}.handle, 'Units', 'pixels');
 set(MW.PG{MW.CounterPB}.handle, 'Position', MW.panelPosition);   
 MW.PB{MW.CounterPB}.handle = uicontrol(MW.panelPB.handle...
     , 'Style', 'radiobutton', 'Units', 'pixels');
-set(MW.PB{MW.CounterPB}.handle, 'Position', [MW.panelPB.d, ...
-       MW.panelPB.height-round((MW.CounterBP)*(MW.h1+MW.d))...
-         , MW.w1-MW.d*2, MW.h1], ...
-           'String', ['<html><b>',Name,' &#9658;</html>']);
-MW.PB{MW.CounterBP}.num = MW.CounterPB;   
-setActivePG(0,0,MW,MW.CounterPB); 
+set(MW.PB{MW.CounterPB}.handle, ...
+       'Position', [MW.panelPB.d, ...
+       MW.panelPB.height-round((MW.CounterPB)* ...
+       (MW.sizePB.height+MW.panelPB.d))-MW.panelPB.d, ...
+       MW.sizePB.width-MW.panelPB.d*2, ...
+       MW.sizePB.height], ...
+       'String', ['<html><b>',name,'']);
+%    'String', ['<html><b>',name,' &#9658;</html>']);
+MW.PB{MW.CounterPB}.num = MW.CounterPB;   
+SetActivePG(0,0,MW,MW.CounterPB); 
 
-function setActivePG(~,~,MW, num)
+function SetActivePG(~,~,MW, num)
     for ii = 1:MW.CounterPB
         if ii ~= num
             set(MW.PG{ii}.handle, 'Visible', 'Off');
+            set(MW.PB{ii}.handle, 'Value', 0);
         else
           	set(MW.PG{ii}.handle, 'Visible', 'On');
+            set(MW.PB{ii}.handle, 'Value', 1);
         end     
     end
   	drawnow;
 
+function ButtonDownCallback(MW)    
+for ii = 1:MW.CounterPB
+    set(MW.PB{ii}.handle,'Callback', {@SetActivePG,MW,ii});  
+end
 
 function closeSPE(name)
 nh = get(0,'children');
@@ -78,4 +98,24 @@ for ii = 1 : length(nh)
         close(nh(ii))
     end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
