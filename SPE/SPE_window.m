8b331d08ad9bbb22d6cd8f993862a659213c701b
mihailovms <mihailovms@mail.ru>
2015-11-24 09:23:00 +0300

    перестал работать (опять) smartGit. продолжаю делать инткрфейс. Рисунки в процессе расчета выводятся на вкладке "Расчет"

--- SPE/SPE_window.m
+++ SPE/SPE_window.m
@@ -1,6 +1,8 @@
 function SPE_window
 %% оконный интерфейс прграммы SPE
-name = 'SPE v0.1.0023';
+name = 'SPE v0.1.0028';
+%% адресс внешней подфункции
+ addpath C:\Git\PuMa3d_mix2
 %% <--- сюда отдельную функцию с параметрами окна:
 % размер, цвет и пр.
 % tic
@@ -12,7 +14,7 @@ SPEEDofLIGHT = 3e8;
 % SaveTemp(MW);
 
 function closeSPE(name)
-nh = get(0,'children');
+nh = allchild(0);
 for ii = 1 : length(nh)
     if strcmp ( get(nh(ii),'name' ), name )
         close(nh(ii))
@@ -24,23 +26,31 @@ function OpenMW(name)
 % все таки считаю нужно избавляться от global
 global MW  % необходимо для корректного CallBack
 closeSPE(name)
-MW.handle = figure(...
+MW.handle = figure(100);
+set(MW.handle,...
     'Name', name,...
     'Units', 'pixels',...
     'Resize', 'off',...
     'NumberTitle', 'off',...
-    'MenuBar', 'none');
+    'MenuBar', 'none',...
+    'DockControls' , 'off', ...
+     'HandleVisibility','on' ...
+    ... % как убрать номер???
+    ); 
 SizeMW;
 PanelPB;
 MW.CounterPB = 0;
 MW.CounterUicEdit = 0;
 MW.CounterUicPB = 0;
+MW.CounterAxes = 0;
 %% <--- кнопки боковой панели
 namePB = 'Входные данные';
     NewPB(namePB);
         CreatePanelNomber1;
     NewPB('Параметры среды');
+        CreatePanelNomber2;
     NewPB('Расчет');
+    CreatePanelNomber3;
 namePB = 'Пост обработка';
     NewPB(namePB);
 namePB = 'Рисунки';
@@ -158,20 +168,66 @@ global MW
 for ii = 1:MW.CounterPB
     set(MW.PB{ii}.handle,'Callback', {@SetActivePG,ii});  
 end
-
+%Problem
 function CreatePanelNomber1
     newUic
     newUicCol
-    newUicMainText( 'Выбор антенны');
+    newUicMainText( 'Параметры задачи');
+        newUicText( 'поляризация' );
+    newUicPopup( {'вертикальная','горизонтальная','правая круговая','правая круговая'} , 'polarization', 1);
+    addCallBack('@NotSet')
     newUicText( 'частота, Гц');
-    newUicEdit( '1.000e+10', 'frequency');
+    newUicEdit( '1.0e+10', 'frequency');
         addCallBack('{@frec2lamda MW.CounterUicEdit+1}');
     newUicText( 'длина волны, м');
-    newUicEdit( '3.000e-2','lamda');
-        addCallBack('{@lamda2frec MW.CounterUicEdit-1}');
+    newUicEdit( '3.0e-2','lambda');
+        addCallBack('{@frec2lamda MW.CounterUicEdit-1}');
     newUicText( 'высота центра антенны, м');
     newUicEdit( '10', 'heightAntenna');
     
+%     newUicMainText( 'Выбор задачи');
+    newUicText( 'Размерность задачи' );
+    newUicPopup( {'2D по вертикали','2D по горизонтали','3D'} , 'dimProblem', 3);
+     newUicMainText( 'Дальность');
+        newUicText( 'начальная дальность, м' );
+    newUicEdit( '20','firstRangeProblem' );
+        newUicText( 'конечная дальность, м' );
+    newUicEdit( '200000','endRangeProblem' );
+        newUicText( 'шаг по дальности, м' );
+    newUicEdit( '1','stepRangeProblem'  );
+        newUicText( 'шаг вывода по дальности, м' );
+    newUicEdit( '10','stepOutRangeProblem'  );
+    newUicMainText( 'Высота');
+        newUicText( '~ высота, м' );
+    newUicEdit( '50','heightProblem' );
+    addCallBack('{@height2log MW.CounterUicEdit+1, MW.CounterUicEdit+2}');
+        newUicText( 'шаг по высоте, м' );
+    newUicEdit( '0.1','stepHeightProblem'  );
+     addCallBack('{@step2log MW.CounterUicEdit-1, MW.CounterUicEdit+1}');
+        newUicText( 'кол-во элементов, log_2' );
+    newUicEdit( '9','log2HeightProblem'  );
+    addCallBack('{@log2height MW.CounterUicEdit-1, MW.CounterUicEdit-2}');
+    newUicMainText( 'Ширина');
+        newUicText( '~ ширина, м' );
+    newUicEdit( '100','widthProblem' );
+    addCallBack('{@height2log MW.CounterUicEdit+1, MW.CounterUicEdit+2}');
+    newUicText( 'шаг по ширине, м' );
+    newUicEdit( '0.1','stepWidthProblem'  );
+     addCallBack('{@step2log MW.CounterUicEdit-1, MW.CounterUicEdit+1}');
+        newUicText( 'кол-во элементов, log_2' );
+    newUicEdit( '10','log2WidthProblem'  );
+    addCallBack('{@log2height MW.CounterUicEdit-1, MW.CounterUicEdit-2}');    
+     newUicCheck( 'увеличение шага по дальности','isAdaptiveStepRange',0);
+      notEnable;
+     newUicText( 'на дальностях, м' );
+    newUicEdit( '100 200 500','adaptiveStepRanges'  );
+  newUicCheck( 'увеличение шага по высоте и ширине','isAdaptiveStepHeight',0);
+   notEnable;
+     newUicText( 'на дальностях, м' );
+    newUicEdit( '100 200 500','adaptiveStepRanges'  );
+        
+        newUicCol
+    newUicMainText( 'Выбор антенны');
     newUicRadio( 'Плоская волна','typeAntenna',0);
         notEnable;
     newUicRadio( 'Дельта-функция','typeAntenna',0);
@@ -179,76 +235,110 @@ function CreatePanelNomber1
     newUicRadio( 'Антенна с заданной ДН','typeAntenna',1);
     newUicText( 'вид ДН антенны');
     newUicPopup( {'<html>sin(x)/x','<html>sin(&theta)'},...
-        'beamTypeAntenna',1);    
-    newUicText(  'ширина ДН, градусов');
-    newUicEdit(  '5', 'beamWithAntenna');
-    newUicText( 'наклон ДН, градусов');
-    newUicEdit(  '0', 'beamTitlAntenna');
+        'beamTypeAntenna',1); 
+    addCallBack('@NotSet')
+    newUicText(  'вертик. ширина ДН, град.');
+    newUicEdit(  '5', 'beamWithAntennaX');
+    newUicText( 'вертик. наклон ДН, град.');
+    newUicEdit(  '0', 'beamTitlAntennaX');
+    newUicText(  'гориз. ширина ДН, град.');
+    newUicEdit(  '5', 'beamWithAntennaY');
+    newUicText( 'гориз. наклон ДН, град.');
+    newUicEdit(  '0', 'beamTitlAntennaY');
+    newUicCheck( 'только главный луч','onlyMainBeam',1);
+    
+    
     newUicRadio( 'АФАР','typeAntenna',0);
     
     newUicMainText( 'Количество элементов решётки')
     newUicText( 'по вертикали');
     newUicEdit(  '100', 'subNx');
+        newUicText( 'шаг по вертикали, lambda');
+    newUicEdit(  '0.5', 'subDx');
     newUicText( 'по горизонтали');
     newUicEdit( '100', 'subNy');
+        newUicText( 'шаг по горизонтали, lambda');
+    newUicEdit(  '0.5', 'subDy');
     newUicText( 'ДН элемента');
-    newUicPopup( {'<html>sin(x)/x','<html>sin(&theta)'},...
-        'beamTypeSubAntenna',2); 
+    newUicPopup( {'<html>sin(&theta)','<html>sin(x)/x'},...
+        'beamTypeSubAntenna',1); 
+    addCallBack('@NotSet')
     
     newUicMainText( 'Направление главного луча')
-    newUicText( 'угол места, градусов');
+    newUicText( 'угол места, град.');
     newUicEdit(  '2', 'beamTitlUMAntenna');
-    newUicText( 'азимут, градусов');
+    newUicText( 'азимут, град.');
     newUicEdit(  '0', 'beamTitlAZAntenna');
-    
-    newUicRadio( 'Зеркальная антенна','typeAntenna',0);
-        notEnable;
-    newUicText( 'площадь, м^2');
-    newUicEdit( '10', 'Sa');
-        notEnable;
-%         newUicText( 'КИП');
-%     newUicEdit( '1', 'KIP');
-    
-    newUicMainText( 'Направление главного луча')
-    newUicText( 'угол места, градусов');
-    newUicEdit(  '2', 'beamTitlUMAntennaZ');
-        notEnable;
-    newUicText( 'азимут, градусов');
-    newUicEdit(  '0', 'beamTitlAZAntennaZ');
-        notEnable;
-    
-    
-    newUicCol
-    newUicMainText( 'Выбор задачи');
-    newUicText( 'Размерность задачи' );
-    newUicPopup( {'2D','3D'} , 'dimProblem',1);
-        newUicText( 'начальная дальность, м' );
-    newUicEdit( '200','firstRangeProblem' );
-        newUicText( 'конечная дальность, м' );
-    newUicEdit( '200000','endRangeProblem' );
-        newUicText( 'шаг по дальности, м' );
-    newUicEdit( '10','stepRangeProblem'  );
-        newUicText( 'высота, м' );
-    newUicEdit( '50','heightProblem' );
-    newUicText( '~ шаг по высоте, м' );
-    newUicEdit( '0.05','stepHeightProblem'  );
-        newUicText( 'ширина, м' );
-    newUicEdit( '200','widthProblem' );
-    newUicText( '~ шаг по ширине, м' );
-    newUicEdit( '0.1','stepWidthProblem'  );
-        newUicCheck( 'адаптивный шаг','adaptiveStepProblem',1);
-        
     
     newUicCol
     newUicMainText( 'Выбор метода');
     newUicCheck( 'метод геометрической оптики','methodGO',0);
-    newUicCheck( 'метод параболического уравнения','methodPE',1);
+    newUicCheck( 'метод параболического уравнения (3D)','methodPE',1);
+    newUicCheck( 'метод параболического уравнения (SUM)','methodSUM',0);
+       notEnable;
     newUicCheck( 'метод поверхностных интегральных уравнений','methodSIE',0);
         notEnable;
     newUicText( 'параметр чего-то' );
     newUicEdit( '0','test' );
+        notEnable; 
+%         newUicText( '' );
+
+        newEmpty
+        newEmpty
+        newEmpty
+   newUicMainText( 'Выбор антенны');     
+       newUicRadio( 'Зеркальная антенна','typeAntenna',0);
         notEnable;
+    newUicText( 'площадь, м^2');
+    newUicEdit( '10', 'Sa');
+        notEnable;
+         newUicText( 'КИП');
+    newUicEdit( '1', 'KIP');
+     notEnable;
     
+    newUicMainText( 'Направление главного луча')
+    newUicText( 'угол места, град.');
+    newUicEdit(  '2', 'beamTitlUMAntennaZ');
+        notEnable;
+    newUicText( 'азимут, град.');
+    newUicEdit(  '0', 'beamTitlAZAntennaZ');
+        notEnable; 
+        
+% environment        
+function CreatePanelNomber2
+    newUic
+    newUicCol
+    newUicMainText( 'Параметры поверхности');
+    newUicText( 'материал поверхности' );
+    newUicPopup( {'идеальный','земля','море'} , 'setMaterialOfSerfase', 1);
+    addCallBack('@NotSet')
+    newUicText( 'диэлектр. проницаемость' );
+      newUicEdit( '-1', 'dielectricOfSerfase');
+      notEnable;
+   newUicMainText( 'выбор неровности');   %undulation
+   newUicRadio( 'гладкая поверхность','typeSerf',1);
+   newUicRadio( 'одиночная неровность','typeSerf',0);
+   
+   newUicCol
+   newUicCheck('мелкомасштабная неровность','isNoiseSerfase',1)
+   newUicText( 'размер шероховатости, м');
+    newUicEdit(  '0.02', 'sizeNoiseSerfase');
+    
+   
+      
+   
+   function CreatePanelNomber3
+       newUic
+       newAxes       
+       newAxes       
+       newUicCol       
+        newAxes        
+        newAxes
+        
+        
+    function newEmpty
+        global MW
+        MW.panel.h = MW.panel.h + 1;
     
 % Добавить кнопки применить, отменить, по умолчанию, сохранить, загрузить 
 function newUicPBs
@@ -265,21 +355,33 @@ newUic
         notEnablePB
 newUicHalfCol
     newUicPB('Рассчитать', 'calc')
+    setCallBack(@Callback_pb2)
+    %     set(MW.PB2{MW.CounterUicPB}.handle,'callback',...
+%         {@Callback_pb2,MW.CounterUicPB})
+
     newUicPB('Пауза', 'pause')
+%      setCallBack(@Callback_pb2)
         notEnablePB
     newUicPB('Стоп', 'stop')
-        notEnablePB
+     setCallBack(@Stop)
+%         notEnablePB
     
 function notEnablePB
         global MW
         MW.PB2{MW.CounterUicPB}.handle.Enable = 'off';    
-
+        
+function setCallBack(func)
+     global MW
+%      MW.PB2{MW.CounterUicPB}.handle.Enable = 'off'; 
+     set(MW.PB2{MW.CounterUicPB}.handle,'callback',...
+     {func,MW.CounterUicPB})
     
 function newUicPB(string, name)    
 global MW
 newUicCol
 position = newUicPositionPB;
  MW.CounterUicPB = MW.CounterUicPB + 1;
+ 
  MW.PB2{MW.CounterUicPB}.handle = ...
         uicontrol( MW.panelPB2.handle,...
         'style','pushbutton',...
@@ -287,12 +389,39 @@ position = newUicPositionPB;
         'FontWeight','bold',...
         'backgroundcolor',MW.panel.pushbackgroundcolor, ...
         'Position', position);    
-    MW.Edit{MW.CounterUicEdit}.name = name;
+%     MW.Edit{MW.CounterUicEdit}.name = name;
     %callback или каждый свой, через eval
     %            или общий и разделение по name или counter
-    set(MW.PB2{MW.CounterUicPB}.handle,'callback',...
-        {@Callback_pb2,MW.CounterUicPB})
-
+%     set(MW.PB2{MW.CounterUicPB}.handle,'callback',...
+%         {@Callback_pb2,MW.CounterUicPB})
+    function newAxes
+        global MW
+        MW.panel.h = MW.panel.h + 1;
+        MW.CounterAxes = MW.CounterAxes + 1;
+        position = newAxesPosition;
+        MW.Ax(MW.CounterAxes) =...
+            axes('parent', MW.PG{MW.CounterPB}.handle,...
+            'position', position);
+        
+        
+    function position = newAxesPosition
+        H = 2; W = 2;
+        global MW
+        h = MW.panel.h;
+        w = MW.panel.w;
+        x0 = 0.1;
+        y0 = 0.1;
+        x = x0 + w/W;
+        y = y0 + (H-h)/H;
+        X = 1/W - 2*x0;
+        Y = 1/H - 2*y0;
+        position = [x, y, X, Y];
+        
+        
+        
+        
+        
+        
 function newUic
 global MW
     MW.panel.h = 0;
@@ -350,16 +479,35 @@ global MW
      
 %      frec2lamda и lamda2frec оказались идентичны!)
 function frec2lamda(a,b,num)
-%         disp('123')
 global MW SPEEDofLIGHT
 MW.Edit{num}.handle.String = ...
-    num2str( SPEEDofLIGHT/str2double(a.String) ,'%10.3e');
+    num2str( SPEEDofLIGHT/str2double(a.String) ,'%10.1e');
              
-function lamda2frec(a,b,num)
-%         disp('321')
-global MW SPEEDofLIGHT
+% function lamda2frec(a,b,num)
+% global MW SPEEDofLIGHT
+% MW.Edit{num}.handle.String = ...
+%     num2str( SPEEDofLIGHT/str2double(a.String) ,'%10.3e');
+
+function height2log(a,b,step,num)
+    global MW
 MW.Edit{num}.handle.String = ...
-    num2str( SPEEDofLIGHT/str2double(a.String) ,'%10.3e');
+    num2str( ceil( log2( ...
+    str2double(a.String)/str2double(MW.Edit{step}.handle.String) ...
+    ) ),'%1.0f');
+
+function log2height(a,b,step,num)
+    global MW
+MW.Edit{num}.handle.String = ...
+    num2str( round(  ...
+    2^str2double(a.String)*str2double(MW.Edit{step}.handle.String) ...
+    ) ,'%1.0f');
+
+function step2log(a,b,num,log)
+    global MW
+    MW.Edit{log}.handle.String = ...
+    num2str( ceil( log2(  ...
+    str2double(MW.Edit{num}.handle.String)/str2double(a.String) ...
+    ) ) ,'%1.0f');
     
 function newUicPopup( string, name, value)
 global MW
@@ -507,21 +655,28 @@ w = MW.panel.w;
         MW.sizePB.height] ;      
  
     
+function  Stop(a,b,CounterUicPB)
+    global STOP
+    STOP = 1;
+    
+    function NotSet(a,b)
+        a.Value = 1;
     
     
 function  Callback_pb2(a,b,CounterUicPB)
 % disp(num2str(CounterUicPB))%
 % считать всю информацию из окна
 % что мы знаем?:
- global MW
+ global MW SPEEDofLIGHT
  % "пока"  вся инфа в Edit. как будет в другой панели???
  for ii = 1 : length(MW.Edit)
 %     disp(MW.Edit{ii}.name)
     if isequal(MW.Edit{ii}.handle.Style,'edit') 
-        eval(['Var.',MW.Edit{ii}.name,' = ',MW.Edit{ii}.handle.String,';'])
+        eval(['Var.',MW.Edit{ii}.name,' = [',MW.Edit{ii}.handle.String,'];'])
     end
     if isequal(MW.Edit{ii}.handle.Style,'popupmenu')
-        eval(['Var.',MW.Edit{ii}.name,' = ',' '' ',MW.Edit{ii}.handle.String{MW.Edit{ii}.handle.Value},' '' ',';'])
+%         eval(['Var.',MW.Edit{ii}.name,' = ',' '' ',MW.Edit{ii}.handle.String{MW.Edit{ii}.handle.Value},' '' ',';'])
+        eval(['Var.',MW.Edit{ii}.name,' = ',num2str(MW.Edit{ii}.handle.Value),';'])
     end
     if isequal(MW.Edit{ii}.handle.Style,'checkbox')
         eval(['Var.',MW.Edit{ii}.name,' = ',num2str(MW.Edit{ii}.handle.Value),';'])
@@ -533,9 +688,14 @@ function  Callback_pb2(a,b,CounterUicPB)
      end
 %     MW.Edit{ii}.handle.Value
  end   
- Var
+ Var.Ax(1) = MW.Ax(2);
+ Var.Ax(2) = MW.Ax(3);
+ Var.Ax(3) = MW.Ax(4);
+ Var.SPEEDofLIGHT = SPEEDofLIGHT;
 %  SPE(var)
- 
+SetActivePG(0, 0, 3)
+
+ PuMa_3D_with_win(Var)
 
 
 
